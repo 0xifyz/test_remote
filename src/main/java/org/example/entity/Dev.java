fc922be8cf52c83a118873e3567503c54d1c7db8
yuanzhan <1942390236@qq.com>
2023-04-06 18:06:19 +0800

    dev提交

---
+++ src/main/java/org/example/entity/Dev.java
@@ -0,0 +1,12 @@
+package org.example.entity;
+
+/**
+ * program: data-migrate
+ * <p>
+ * 功能描述: dev分支的内容
+ *
+ * @author : YuanZ
+ * @since : 2023-04-06 17:39
+ **/
+public class Dev {
+}
---
+++ src/main/java/org/example/entity/Dev2.java
@@ -0,0 +1,12 @@
+package org.example.entity;
+
+/**
+ * program: data-migrate
+ * <p>
+ * 功能描述:
+ *
+ * @author : YuanZ
+ * @since : 2023-04-06 17:43
+ **/
+public class Dev2 {
+}
